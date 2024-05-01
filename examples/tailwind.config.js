cb5edc86ea805d65da2c78d69db41fa17359ad14
Martossy Alex <github@mail.martossy.hu>
2024-05-01 15:43:10 +0200

    Add changeset

--- examples/tailwind.config.js
+++ examples/tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['index.html'],
-  plugins: [require('../src')()],
+  plugins: [require('../dist/index.mjs')()],
   theme: {
     extend: {
       colors: {
