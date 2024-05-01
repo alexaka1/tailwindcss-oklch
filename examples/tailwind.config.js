e3979a4a55568224a187f3e820eadba73f35d9ce
Martossy Alex <github@mail.martossy.hu>
2024-05-01 15:45:48 +0200

    Revert example project

--- examples/tailwind.config.js
+++ examples/tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['index.html'],
-  plugins: [require('../dist/index.mjs')()],
+  plugins: [require('../src')()],
   theme: {
     extend: {
       colors: {
