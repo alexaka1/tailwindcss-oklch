e9fc9f8420910dca1d6da336c836bed028eab6cb
Martossy Alex <github@mail.martossy.hu>
2024-05-01 16:29:53 +0200

    revert example for now

--- examples/tailwind.config.js
+++ examples/tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
   content: ['index.html'],
-  plugins: [require('@alexaka1/tailwindcss-oklch')()],
+  plugins: [require('../src/tailwindcss-oklch/src')()],
   theme: {
     extend: {
       colors: {
